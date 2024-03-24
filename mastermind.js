2bb32d9f603b55bd54758a98c7341fe08681b156
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-03-24 14:44:43 -0400

    Update mastermind.js

--- mastermind.js
+++ mastermind.js
@@ -7,17 +7,35 @@ class Mastermind {
 
     generateSecretCode() {
         // Generate a random secret code
+        let code = '';
+        const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']; // Example colors
+        for (let i = 0; i < 4; i++) {
+            const randomIndex = Math.floor(Math.random() * colors.length);
+            code += colors[randomIndex][0]; // Take the first letter of each color as code
+        }
+        return code;
     }
 
     checkGuess(guess) {
         // Check the guess against the secret code
+        let correct = '';
+        for (let i = 0; i < guess.length; i++) {
+            if (guess[i] === this.secretCode[i]) {
+                correct += 'R'; // Right color and right position
+            } else if (this.secretCode.includes(guess[i])) {
+                correct += 'W'; // Right color but wrong position
+            } else {
+                correct += 'X'; // Wrong color
+            }
+        }
+        return feedback;
     }
 
     isGameOver() {
         // Check if the game is over
+        return this.currentAttempt >= this.maxAttempts || this.checkGuess(guess) === 'RRRR';
     }
-
-    // Other methods...
+    
 }
 
 class UI {
@@ -45,6 +63,7 @@ class Game {
 
     start() {
         // Game initialization logic
+        this.ui.displayMessage('Welcome to Mastermind! Try to guess the colors! There are four same/different colors in each code (red, blue, green, yellow, purple, orange). Guess it in under ten guesses and you win! * means correct color but wrong loaction. ^ means correct color and location. X means wrong color and wrong location. Have fun!';
     }
 
     // Other game related methods...
